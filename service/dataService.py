b487981ca750cc4e6aaefa852c9ddea924160a86
KFClpy <2291897613@qq.com>
2023-04-16 12:33:58 +0800

    datalist

--- service/dataService.py
+++ service/dataService.py
@@ -7,6 +7,8 @@ from mysqldb.models import DataTableModel
 def get_data_name(username):
     data_name_list = db.session.query(distinct(DataTableModel.dataname)).filter(
         DataTableModel.username == username).all()
+    result_list=[]
     if len(data_name_list) != 0:
-        data_name_list = data_name_list[0]
-    return list(data_name_list)
+        for data_name in data_name_list:
+            result_list.append(data_name[0])
+    return result_list
