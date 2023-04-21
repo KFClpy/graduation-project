cf3500949b4ed601ecb1eeb070b8f93580cc6a5f
KFClpy <2291897613@qq.com>
2023-04-21 15:14:05 +0800

    manual

--- manual_fuzzy_join/manual_join.py
+++ manual_fuzzy_join/manual_join.py
@@ -6,9 +6,6 @@ from manual_fuzzy_join.join_funcion.tokenizer import Tokenizer
 
 
 def manualJoin(data_left, data_right, config):
-    print(config['preprocessor'])
-    print(config['tokenizer'])
-    print(config['distance_function'])
     tokenizer = Tokenizer(config['tokenizer'])
     preprocessor = Preprocessor(config['preprocessor'])
     left_title = data_left['title']
@@ -20,23 +17,28 @@ def manualJoin(data_left, data_right, config):
     left_id = data_left['id']
     right_id = data_right['id']
     left_result = {}
-    for nowId in left_id:
-        left_result[nowId] = []
+    for index,value in left_id.items():
+        left_result[value] = []
     distance_functions = config['distance_function']
-    for key, value in distance_functions:
+    for key, value in distance_functions.items():
         threshold = value
         distance_function = key
-        for left_child in left_tokenizer:
-            child_join(left_child, right_tokenizer, threshold, distance_function)
+        for i, v in left_tokenizer.items():
+            result_id = child_join(v, right_tokenizer, threshold, distance_function, right_id)
+            index_id = left_id[i]
+            if result_id is None:
+                continue
+            left_result[index_id].append(result_id)
+    return left_result
 
 
-def child_join(left_title, series_right, threshold, distance_function,right_id):
+def child_join(left_title, series_right, threshold, distance_function, right_id):
     distanceFunction = DistanceFunction(distance_function)
     df = DataFrame()
-    df['value_l'] = left_title
     df['value_r'] = series_right
+    df['value_l'] = [left_title] * len(df.index)
     distance_series = distanceFunction.compute_distance(df)
-    minId=right_id[distance_series.idxmin()]
-    if distance_series.min()>threshold:
-        minId=None
+    minId = right_id[distance_series.idxmin()]
+    if distance_series.min() > threshold:
+        minId = None
     return minId
--- test/test.py
+++ test/test.py
@@ -10,22 +10,13 @@ from manual_fuzzy_join.manual_join import manualJoin
 from run import app
 
 with app.app_context():
-    # preprocessor = Preprocessor("lowerRemovePunctuationStem")
-    # tokenizer = Tokenizer("threeGram")
-    # distanceFunction = DistanceFunction("jaccardDistance")
-    # df = pd.read_csv(Path.CSV_PATH + "/data_generate.csv")
-    # column_pre_left = preprocessor.preprocess(df['title_l'])
-    # column_pre_right = preprocessor.preprocess(df['title_r'])
-    # column_token_left = tokenizer.tokenize(column_pre_left)
-    # column_token_right = tokenizer.tokenize(column_pre_right)
-    # df=DataFrame()
-    # df['value_r']=column_token_right
-    # df['value_l']=column_token_left
-    # distance = distanceFunction.compute_distance(df)
-    # print(distance)
+    df_left=pd.read_csv("F:/AutomaticFuzzyJoin-master/src/autofj/benchmark/TennisTournament/left.csv")
+    df_right=pd.read_csv("F:/AutomaticFuzzyJoin-master/src/autofj/benchmark/TennisTournament/right.csv")
     data = {
-        "preprocessor": " ",
-        "tokenizer": " ",
-        "distance_function": []
+        "preprocessor": "lowerRemovePunctuationStem",
+        "tokenizer": "threeGram",
+        "distance_function": {
+            "jaccardDistance":0.5
+        }
     }
-    manualJoin(DataFrame(),DataFrame(),config=data)
+    print(manualJoin(df_left,df_right,config=data))
