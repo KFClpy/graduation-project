b3c0270e93dfa37064f86c3f1b0cb9343e85bb1c
KFClpy <2291897613@qq.com>
2023-04-21 18:38:15 +0800

    dataSet

---
+++ service/dataSetService.py
@@ -0,0 +1,18 @@
+from sqlalchemy import distinct, func
+
+from mysqldb.exts import db
+from mysqldb.models import DataMappingModel, DataTableModel
+
+
+def get_dataSet_info(username):
+    dataname_mappings = db.session.query(DataMappingModel.dataname, func.count(DataMappingModel.th_name)).filter(
+        DataMappingModel.username == username).group_by(DataMappingModel.dataname).all()
+    dataname_count = db.session.query(DataTableModel.dataname, func.count(DataTableModel.tid)). \
+        filter(DataTableModel.username == username).group_by(DataTableModel.dataname).all()
+    data_name = {}
+    for value in dataname_mappings:
+        data_name[value[0]] = []
+        data_name[value[0]].append(value[1])
+    for value in dataname_count:
+        data_name[value[0]].append(value[1])
+    return data_name
--- test/test.py
+++ test/test.py
@@ -8,15 +8,21 @@ from manual_fuzzy_join.join_funcion.preprocessor import Preprocessor
 from manual_fuzzy_join.join_funcion.tokenizer import Tokenizer
 from manual_fuzzy_join.manual_join import manualJoin
 from run import app
+from service.dataSetService import get_dataSet_info
 
 with app.app_context():
-    df_left=pd.read_csv("F:/AutomaticFuzzyJoin-master/src/autofj/benchmark/TennisTournament/left.csv")
-    df_right=pd.read_csv("F:/AutomaticFuzzyJoin-master/src/autofj/benchmark/TennisTournament/right.csv")
-    data = {
-        "preprocessor": "lowerRemovePunctuationStem",
-        "tokenizer": "threeGram",
-        "distance_function": {
-            "jaccardDistance":0.5
-        }
-    }
-    print(manualJoin(df_left,df_right,config=data))
+    # df_left=pd.read_csv("F:/AutomaticFuzzyJoin-master/src/autofj/benchmark/TennisTournament/left.csv")
+    # df_right=pd.read_csv("F:/AutomaticFuzzyJoin-master/src/autofj/benchmark/TennisTournament/right.csv")
+    # data = {
+    #     "preprocessor": "lowerRemovePunctuationStem",
+    #     "tokenizer": "threeGram",
+    #     "distance_function": {
+    #         "jaccardDistance":0.5,
+    #         "editDistance":0.5
+    #     }
+    # }
+    # for key,value in manualJoin(df_left,df_right,config=data).items():
+    #     if value:
+    #         print(key,value)
+    print(get_dataSet_info("admin123"))
+
