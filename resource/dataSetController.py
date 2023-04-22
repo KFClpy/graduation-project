fa27d877881039014b7b7aadb056b959724d119e
KFClpy <2291897613@qq.com>
2023-04-22 16:57:43 +0800

    deleteOneColumn

--- resource/dataSetController.py
+++ resource/dataSetController.py
@@ -3,14 +3,14 @@ from flask_restful import Resource, reqparse
 
 from base.baseview import BaseView
 from base.status_code import Codes
-from service.dataSetService import get_dataset_info, delete_dataset_info, delete_column
+from service.dataSetService import get_dataset_info, delete_dataset_info, delete_column, get_column_info
 from utils.logger import base_log
 
 dataSetParser = reqparse.RequestParser()
 dataSetParser.add_argument("data_name", required=True, help="数据集名称不允许为空")
 dataDeleteParser = reqparse.RequestParser()
 dataDeleteParser.add_argument("data_name", required=True, help="数据集名称不允许为空")
-dataDeleteParser.add_argument("column_name", required=True, help="列名不许为空")
+dataDeleteParser.add_argument("column_id", required=True, help="列id不许为空")
 
 
 class GetDataInfo(Resource, BaseView):
@@ -46,13 +46,26 @@ class DeleteOneColumn(Resource, BaseView):
     def post(self):
         user_name = get_jwt_identity()
         data_name = dataDeleteParser.parse_args()['data_name']
-        column_name = dataDeleteParser.parse_args()['column_name']
+        column_id = dataDeleteParser.parse_args()['column_id']
         try:
             data = {
                 "username": user_name
             }
-            delete_column(user_name, data_name, column_name)
+            delete_column(user_name, data_name, column_id)
             return self.formattingData(code=Codes.SUCCESS.code, msg=Codes.SUCCESS.desc, data=data)
         except Exception as e:
             base_log.info(e)
             return self.formattingData(code=Codes.FAILE.code, msg=Codes.FAILE.desc, data=None)
+
+
+class GetColumnInfo(Resource,BaseView):
+    @jwt_required()
+    def post(self):
+        user_name=get_jwt_identity()
+        data_name=dataSetParser.parse_args()['data_name']
+        try:
+            data=get_column_info(user_name,data_name)
+            return self.formattingData(code=Codes.SUCCESS.code, msg=Codes.SUCCESS.desc,data=data)
+        except Exception as e:
+            base_log.info(e)
+            return self.formattingData(code=Codes.FAILE.code,msg=Codes.FAILE.desc,data=None)
--- service/dataSetService.py
+++ service/dataSetService.py
@@ -34,17 +34,18 @@ def delete_dataset_info(username, dataname):
         raise e
 
 
-def delete_column(username, dataname, columnname):
+def delete_column(username, dataname, columnid):
     try:
         count = db.session.query(func.count(DataMappingModel.th_id)). \
             filter(DataMappingModel.username == username, DataMappingModel.dataname == dataname).first()[0]
         if count > 1:
             db.session.query(DataMappingModel).filter(DataMappingModel.username == username,
                                                       DataMappingModel.dataname == dataname,
-                                                      DataMappingModel.th_name == columnname).delete()
-        else :
-            if db.session.query(DataMappingModel.th_name).filter(DataMappingModel.dataname==dataname,
-                                                                 DataMappingModel.username==username).first()[0]==columnname:
+                                                      DataMappingModel.th_id == columnid).delete()
+        else:
+            if db.session.query(DataMappingModel.th_id).filter(DataMappingModel.dataname == dataname,
+                                                               DataMappingModel.username == username). \
+                    first()[0] == int(columnid):
                 db.session.query(DataTableModel).filter(DataTableModel.dataname == dataname,
                                                         DataTableModel.username == username).delete()
                 db.session.query(DataMappingModel).filter(DataMappingModel.dataname == dataname,
@@ -53,3 +54,13 @@ def delete_column(username, dataname, columnname):
     except Exception as e:
         db.session.rollback()
         raise e
+
+
+def get_column_info(username, dataname):
+    datas = db.session.query(DataMappingModel).filter(
+        DataMappingModel.username == username, DataMappingModel.dataname == dataname).all()
+    result = []
+    for data in datas:
+        now = {"column_id": data.th_id, "column_name": data.th_name}
+        result.append(now)
+    return result
